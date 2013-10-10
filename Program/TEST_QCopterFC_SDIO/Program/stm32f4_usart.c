6af09624e585ca978f60fbbbb8383e8e328f0a6f
Hom <Hom19910422@gmail.com>
2013-10-10 19:08:16 +0800

    Update TEST_QCopterFC_SDIO 10/10

--- Program/TEST_QCopterFC_SDIO/Program/stm32f4_usart.c
+++ Program/TEST_QCopterFC_SDIO/Program/stm32f4_usart.c
@@ -4,72 +4,100 @@
 #include "stm32f4_usart.h"
 /*=====================================================================================================*/
 /*=====================================================================================================*
-**��� : NumToChar
-**�\�� : 
-**��J : 
-**��X : 
-**�ϥ� : NumToChar(Type_O, 6, TrData, Data)
+**函數 : USART_SendByte
+**功能 : 發送 1Byte 資料
+**輸入 : USARTx, *SendData
+**輸出 : None
+**使用 : USART_SendByte(USART1, 'A');
 **=====================================================================================================*/
 /*=====================================================================================================*/
-void NumToChar( u8 Type, u8 Num, u8 *CharData, s32 NumData )
+void USART_SendByte( USART_TypeDef* USARTx, uc8 *SendData )
 {
-	u8 i = 0;
-	u32 Tmp[48] = {0};
-	u32 NumTmp = 1;
+  USART_SendData(USARTx, *SendData);
+  while(USART_GetFlagStatus(USARTx, USART_FLAG_TXE) == RESET);
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*
+**函數 : USART_RecvByte
+**功能 : 接收 1Byte 資料
+**輸入 : USARTx
+**輸出 : RecvData
+**使用 : RecvData = USART_RecvByte(USART1);
+**=====================================================================================================*/
+/*=====================================================================================================*/
+u16 USART_RecvByte( USART_TypeDef* USARTx )
+{
+  while(USART_GetFlagStatus(USARTx, USART_FLAG_RXNE) == RESET);
 
-	switch(Type) {
+  return USART_ReceiveData(USARTx);
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*
+**函數 : NumToChar
+**功能 : 數值轉字串
+**輸入 : Type, NumLen, *CharData, NumData
+**輸出 : None
+**使用 : NumToChar(Type_O, 6, TrData, Data)
+**=====================================================================================================*/
+/*=====================================================================================================*/
+void NumToChar( u8 Type, u8 NumLen, u8 *CharData, s32 NumData )
+{
+  u8 i = 0;
+  u32 Tmp[48] = {0};
+  u32 NumTmp = 1;
 
-		case Type_B:
-		case Type_O:
-		case Type_D:
-		case Type_H:
-			for(i=0; i<Num; i++) {
-				Tmp[i] = ((u32)NumData) / NumTmp;
-				NumTmp = NumTmp * Type;
-			}
-			for(i=0; i<Num; i++) {
-				CharData[Num-i-1] = Tmp[i] - Tmp[i+1]*Type;
-				if(CharData[Num-i-1]>9)
-					CharData[Num-i-1] += 55;	// 65-10
-				else
-					CharData[Num-i-1] += 48;
-			}
-			CharData[Num] = '\0';
-			break;
+  switch(Type) {
 
-		case Type_I:
-			if(NumData<0) {
-				CharData[0] = '-';
-				NumData = (~NumData) + 1;
-				for(i=1; i<Num+1; i++) {
-					Tmp[i-1] = ((u32)NumData) / NumTmp;
-					NumTmp = NumTmp * 10;
-				}
-				for(i=1; i<Num+1; i++) {
-					CharData[Num-i+1] = Tmp[i-1] - Tmp[i]*10;
-					CharData[Num-i+1] += 48;
-				}
-				CharData[Num+1] = '\0';
-			}
-			else {
-				CharData[0] = '+';
-				//NumData = (~NumData) + 1;
-				for(i=1; i<Num+1; i++) {
-					Tmp[i-1] = ((u32)NumData) / NumTmp;
-					NumTmp = NumTmp * 10;
-				}
-				for(i=1; i<Num+1; i++) {
-					CharData[Num-i+1] = Tmp[i-1] - Tmp[i]*10;
-					CharData[Num-i+1] += 48;
-				}
-				CharData[Num+1] = '\0';
-			}
-			break;
+    case Type_B:
+    case Type_O:
+    case Type_D:
+    case Type_H:
+      for(i=0; i<NumLen; i++) {
+        Tmp[i] = ((u32)NumData) / NumTmp;
+        NumTmp = NumTmp * Type;
+      }
+      for(i=0; i<NumLen; i++) {
+        CharData[NumLen-i-1] = Tmp[i] - Tmp[i+1]*Type;
+        if(CharData[NumLen-i-1]>9)
+          CharData[NumLen-i-1] += 55;	// 65-10
+        else
+          CharData[NumLen-i-1] += 48;
+      }
+      CharData[NumLen] = '\0';
+    break;
 
-// 		case Type_F:
-// 				
-// 			break;
-	}
+    case Type_I:
+      if(NumData<0) {
+        CharData[0] = '-';
+        NumData = (~NumData) + 1;
+        for(i=1; i<NumLen+1; i++) {
+          Tmp[i-1] = ((u32)NumData) / NumTmp;
+          NumTmp = NumTmp * 10;
+        }
+        for(i=1; i<NumLen+1; i++) {
+          CharData[NumLen-i+1] = Tmp[i-1] - Tmp[i]*10;
+          CharData[NumLen-i+1] += 48;
+        }
+        CharData[NumLen+1] = '\0';
+      }
+      else {
+        CharData[0] = '+';
+        //NumData = (~NumData) + 1;
+        for(i=1; i<NumLen+1; i++) {
+          Tmp[i-1] = ((u32)NumData) / NumTmp;
+          NumTmp = NumTmp*10;
+        }
+        for(i=1; i<NumLen+1; i++) {
+          CharData[NumLen-i+1] = Tmp[i-1] - Tmp[i]*10;
+          CharData[NumLen-i+1] += 48;
+        }
+        CharData[NumLen+1] = '\0';
+      }
+      break;
+
+    case Type_F:
+      break;
+  }
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
