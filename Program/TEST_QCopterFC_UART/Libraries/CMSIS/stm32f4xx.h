2f9049035f84c2a8aa124a8b2a4ab77038efe30e
Hom <Hom19910422@gmail.com>
2014-01-18 01:44:06 +0800

    Update TestUART 01/18
    define float and double ... fp32 & fp64

--- Program/TEST_QCopterFC_UART/Libraries/CMSIS/stm32f4xx.h
+++ Program/TEST_QCopterFC_UART/Libraries/CMSIS/stm32f4xx.h
@@ -468,37 +468,59 @@ typedef enum IRQn
   * @{
   */  
 /*!< STM32F10x Standard Peripheral Library old types (maintained for legacy purpose) */
-typedef int32_t  s32;
+typedef int64_t s64;
+typedef int32_t s32;
 typedef int16_t s16;
 typedef int8_t  s8;
 
+typedef const int64_t sc64;  /*!< Read Only */
 typedef const int32_t sc32;  /*!< Read Only */
 typedef const int16_t sc16;  /*!< Read Only */
-typedef const int8_t sc8;   /*!< Read Only */
+typedef const int8_t  sc8;   /*!< Read Only */
 
-typedef __IO int32_t  vs32;
-typedef __IO int16_t  vs16;
-typedef __IO int8_t   vs8;
+typedef __IO int64_t vs64;
+typedef __IO int32_t vs32;
+typedef __IO int16_t vs16;
+typedef __IO int8_t  vs8;
 
+typedef __I int64_t vsc64;  /*!< Read Only */
 typedef __I int32_t vsc32;  /*!< Read Only */
 typedef __I int16_t vsc16;  /*!< Read Only */
-typedef __I int8_t vsc8;   /*!< Read Only */
+typedef __I int8_t  vsc8;   /*!< Read Only */
 
-typedef uint32_t  u32;
+typedef uint64_t u64;
+typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t  u8;
 
+typedef const uint64_t uc64;  /*!< Read Only */
 typedef const uint32_t uc32;  /*!< Read Only */
 typedef const uint16_t uc16;  /*!< Read Only */
-typedef const uint8_t uc8;   /*!< Read Only */
+typedef const uint8_t  uc8;   /*!< Read Only */
 
-typedef __IO uint32_t  vu32;
+typedef __IO uint64_t vu64;
+typedef __IO uint32_t vu32;
 typedef __IO uint16_t vu16;
 typedef __IO uint8_t  vu8;
 
+typedef __I uint64_t vuc64;  /*!< Read Only */
 typedef __I uint32_t vuc32;  /*!< Read Only */
 typedef __I uint16_t vuc16;  /*!< Read Only */
-typedef __I uint8_t vuc8;   /*!< Read Only */
+typedef __I uint8_t  vuc8;   /*!< Read Only */
+
+typedef float  fp32;
+typedef double fp64;
+
+typedef const float  fpc32;
+typedef const double fpc64;
+
+typedef __IO float  vfp32;
+typedef __IO double vfp64;
+
+typedef __I float  vfpc32;
+typedef __I double vfpc64;
+
+typedef enum {FALSE = 0, TRUE = !FALSE} bool;
 
 typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus;
 
